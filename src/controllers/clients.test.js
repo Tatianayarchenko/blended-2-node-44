9967f4561f15c01384772b6658bdd6caf174063b
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-11-13 12:33:13 +0200

    added mockGetByIdService, beforeAll, afterEach, it for not existed id

--- src/controllers/clients.test.js
+++ src/controllers/clients.test.js
@@ -1,11 +1,21 @@
-const services = require('../services/clients');
-const { getById } = require('../controllers/clients');
+const services = require("../services/clients");
+const { getById } = require("../controllers/clients");
 
-describe('getById', () => {
-  it('It should call res.json() with needed document, if it gets existed id', async () => {
+describe("getById", () => {
+  let mockGetByIdService;
+
+  beforeAll(() => {
+    mockGetByIdService = jest.spyOn(services, "getByIdService");
+  });
+
+  afterEach(() => {
+    mockGetByIdService.mockReset();
+  });
+
+  it("It should call res.json() with needed document, if it gets existed id", async () => {
     const req = {
       params: {
-        id: '635e4ec9b2348c6bf7fd1068',
+        id: "635e4ec9b2348c6bf7fd1068",
       },
     };
 
@@ -14,17 +24,40 @@ describe('getById', () => {
     };
 
     const neededDocument = {
-      name: 'Yuriy',
-      surname: 'Pokgyl',
-      email: 'mail2@mail.com',
+      name: "Yuriy",
+      surname: "Pokgyl",
+      email: "mail2@mail.com",
       status: true,
     };
 
-    jest.spyOn(services, 'getByIdService').mockResolvedValue(neededDocument);
+    mockGetByIdService.mockResolvedValue(neededDocument);
 
     await getById(req, res);
 
     expect(res.json).toHaveBeenCalled();
     expect(res.json.mock.calls[0][0].client).toEqual(neededDocument);
+  });
+
+  it('it should call res with status 404 and json "Not found", if it gets not existed id', async () => {
+    const req = {
+      params: {
+        id: "635e4ec9b2348c6bf7fd1065",
+      },
+    };
+
+    const json = jest.fn();
+
+    const res = {
+      status: jest.fn(() => ({ json })),
+    };
+
+    mockGetByIdService.mockResolvedValue(null);
+
+    await getById(req, res);
+
+    expect(res.status).toHaveBeenCalled();
+    expect(res.status.mock.calls[0][0]).toEqual(404);
+    expect(json).toHaveBeenCalled();
+    expect(json.mock.calls[0][0]).toEqual({ message: "Not found" });
   });
 });
